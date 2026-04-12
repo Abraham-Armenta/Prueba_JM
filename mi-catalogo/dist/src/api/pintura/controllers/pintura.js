f3e69d6f9026c5b463967c970845d01fc79acfd0
Abraham Armenta <tu_correo_de_github@gmail.com>
2026-04-11 17:11:30 -0700

    primer commit

---
+++ mi-catalogo/dist/src/api/pintura/controllers/pintura.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * pintura controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::pintura.pintura');
